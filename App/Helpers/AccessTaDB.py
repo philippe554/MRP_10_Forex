a82e49fc7bd794e6906bd76ffa7fcc7ff9aa70ae
Rodrigo Brechard <rodrigobrechard@gmail.com>
2018-11-23 10:23:57 +0100

    Retrieve window with offset and windows size implemented. TrendAnalysis methods implemented.

--- App/Helpers/AccessTaDB.py
+++ App/Helpers/AccessTaDB.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import sqlite3
 import os
-import matplotlib.pyplot as plt
 
 inputFile = os.path.expanduser('~/Google Drive/Group 10_ Forecast the FOREX market/Data/price_hist_ta.db')
 
@@ -13,12 +12,13 @@ class AccessDB:
         self.offset = offset
         self.size = 300000
 
-    """
-    Get columns from the database with all the technical indicators
-    @columns Array of strings with the name of the columns to extract
-    @return Pandas object with the required columns
-    """
     def get_column(self, columns):
+        """
+        Get columns from the database with all the technical indicators
+
+        :param columns: Array of strings with the name of the columns to extract
+        :return: Pandas object with the required columns
+        """
         done = False
         new_offset = self.offset
         while not done:
@@ -38,13 +38,14 @@ class AccessDB:
             new_offset += self.size
         return df
 
-    """
-    Get columns from the database with all the technical indicators only for a window at the end of the DB
-    @columns Array of strings with the name of the columns to extract
-    @return Pandas object with the required columns
-    """
     def get_end_window_column(self, columns, window_size):
+        """
+        Get columns from the database with all the technical indicators only for a window at the end of the DB
 
+        :param columns: Array of strings with the name of the columns to extract
+        :param window_size: Size of the window to retrieve
+        :return: Pandas object with the required columns
+        """
         done = False
 
         conn = sqlite3.connect(inputFile)
@@ -69,4 +70,51 @@ class AccessDB:
 
             new_offset += self.size
 
+        return df
+
+    def get_window_column(self, columns, offset, window_size):
+        """
+        Get columns from the database with all the technical indicators only for a window
+
+        :param columns: Array of strings with the name of the columns to extract
+        :param offset: Start point of the window to retrieve
+        :param window_size: Size of the window to retrieve
+        :return: Pandas object with the required columns
+        """
+
+        done = False
+
+        conn = sqlite3.connect(inputFile)
+
+        first_offset = offset
+        new_offset = first_offset
+
+        end_window = offset + window_size
+        # The limit starts as the created in the constructor
+        limit = self.size
+
+        while not done:
+            # If the number of rows left to be searched is smaller than the limit of the constructor
+            if end_window - new_offset < self.size:
+                # Set the limit to amount of rows left to search
+                limit = end_window - new_offset
+
+            # print("next batch:", new_offset.__str__(), "-", limit)
+
+            conn = sqlite3.connect(inputFile)
+
+            new_value = pd.read_sql_query("SELECT " + ','.join(columns) + " FROM price_hist LIMIT "
+                                          + limit.__str__() + " OFFSET " + new_offset.__str__(), conn)
+
+            if new_offset is first_offset:
+                df = new_value
+            else:
+                df = df.append(new_value, ignore_index=True)
+
+            if new_value.size < self.size:
+                done = True
+
+            new_offset += self.size
+            if new_offset > end_window:
+                break
         return df
--- App/Scripts/TA/TrendAnalisys.py
+++ App/Scripts/TA/TrendAnalisys.py
@@ -3,8 +3,112 @@ from App.Helpers.AccessTaDB import AccessDB
 import matplotlib.pyplot as plt
 
 
-def get_MACD(offset=0):
-    db = AccessDB(offset)
-    return db.get_end_window_column(["trend_macd_diff"], 500000)
+class TrenAnalisys:
 
-print(get_MACD(5000000).tail())
+    def __init__(self):
+        self.db = AccessDB()
+
+    def get_MACD(self, offset, window_size):
+        """
+        Moving Average Convergence Divergence (MACD) is a trend-following momentum indicator. The signal of a change
+        of trend is given with the crossover of the values of MACD, therefore, we retrieve the difference between them.
+
+        :param offset: offset of the window
+        :param window_size: size of the window
+        :return: pandas with the column: trend_macd_diff
+        """
+        return self.db.get_window_column(["trend_macd_diff"], offset, window_size)
+
+    def get_ADX(self, offset, window_size):
+        """
+        Get the Average Directional Index, a momentum strength indicators. The ADX identifies a strong positive trend
+        when the ADX is over 25 and a weak trend when the ADX is below 20.
+
+        :param offset: offset of the window
+        :param window_size: size of the window
+        :return: pandas with the column: trend_adx
+        """
+        return self.db.get_window_column(["trend_adx"], offset, window_size)
+
+    def get_VI(self, offset, window_size):
+        """
+        Get the vortex indicator values. It is composed of 2 lines that show both positive (VI +) and negative (VI -)
+        trend movement. The signal of a change of trend is given when the lines cross, therefore, we retrieve the
+        difference between them.
+
+        :param offset: offset of the window
+        :param window_size: size of the window
+        :return: pandas with the column: trend_vortex_diff
+        """
+        return self.db.get_window_column(["trend_vortex_diff"], offset, window_size)
+
+    def get_TRIX(self, offset, window_size):
+        """
+        Get the TRIX values. It is designed to filter out price movements that are considered insignificant or
+        unimportant.
+
+        :param offset: offset of the window
+        :param window_size: size of the window
+        :return: pandas with the column: trend_trix
+        """
+        return self.db.get_window_column(["trend_trix"], offset, window_size)
+
+    def get_MI(self, offset, window_size):
+        """
+        Get Mass index. This indicator examines the range between high and low stock prices over a period of time.
+
+        :param offset: offset of the window
+        :param window_size: size of the window
+        :return: pandas with the column: trend_mass_index
+        """
+        return self.db.get_window_column(["trend_mass_index"], offset, window_size)
+
+    def get_CCI(self, offset, window_size):
+        """
+        Get the Commodity Channel Index. Momentum-based technical trading tool used most often to help determine when
+        an investment vehicle is reaching a condition of being overbought or oversold.
+
+        :param offset: offset of the window
+        :param window_size: size of the window
+        :return: pandas with the column: trend_cci
+        """
+        return self.db.get_window_column(["trend_cci"], offset, window_size)
+
+    def get_DPO(self, offset, window_size):
+        """
+        Get the Detrended Price Oscillator. Oscillator that strips out price trends in an effort to estimate the length
+        of price cycles from peak to peak, or trough to trough.
+
+        :param offset: offset of the window
+        :param window_size: size of the window
+        :return: pandas with the column: trend_dpo
+        """
+        return self.db.get_window_column(["trend_dpo"], offset, window_size)
+
+
+    def get_KST(self, offset, window_size):
+        """
+        In Know Sure Thing indicator it is important not only the crossover but also the trend of the indicator itself.
+        For this reason, the three values are needed.
+
+        :param offset: offset of the window
+        :param window_size: size of the window
+        :return: pandas with the columns: trend_kst, trend_kst_sig, trend_kst_diff
+        """
+        return self.db.get_window_column(["trend_kst, trend_kst_sig, trend_kst_diff"], offset, window_size)
+
+    def get_Ichimoku(self, offset, window_size):
+        """
+        Get the values of the Ichimoku cloud. Shows support and resistance, and momentum and trend directions.
+
+        :param offset: offset of the window
+        :param window_size: size of the window
+        :return: pandas with the columns: trend_ichimoku_a, trend_ichimoku_b
+        """
+        return self.db.get_window_column(["trend_ichimoku_a, trend_ichimoku_b"], offset, window_size)
+
+
+t_an = TrenAnalisys()
+
+print(t_an.get_Ichimoku(100, 100))
+# print(t_an.get_Ichimoku(0,100))
