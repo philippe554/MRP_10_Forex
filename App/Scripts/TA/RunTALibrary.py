239ab33d500a966049645b621f1140e314a58a01
Omendra Manhar <omendra.manhar@gmail.com>
2018-11-27 16:06:28 +0100

    Added Volume TA

--- App/Scripts/TA/RunTALibrary.py
+++ App/Scripts/TA/RunTALibrary.py
@@ -9,8 +9,6 @@ import sqlite3
 inputFile = '../../Data/price_hist.db'
 targetFile = 'price_hist_ta.db'
 
-
-
 done = False
 # offset = 0 #next batch: 560000
 offset = 560000
@@ -19,24 +17,22 @@ while not done:
     print("next batch: " + offset.__str__())
     conn = sqlite3.connect(inputFile)
     # df = pd.read_sql_query("SELECT * FROM price_hist LIMIT 10 OFFSET 6000000", conn)
-    df = pd.read_sql_query("SELECT * FROM price_hist LIMIT "+size.__str__()+" OFFSET "+ offset.__str__(), conn)
+    df = pd.read_sql_query("SELECT * FROM price_hist LIMIT "+ size.__str__()+" OFFSET "+ offset.__str__(), conn)
     offset += size
 
-    if df.shape != (0,7):
+    print(df.columns)
+
+    if df.shape != (0, 7):
         # Add all ta features filling nans values
-        open = "BarOPEMBid"
+        open = "BarOPENBid"
         high = "BarHIGHBid"
         low = "BarLOWBid"
         close = "BarCLOSEBid"
         fillna = True
-        # df = add_all_ta_features(df, "Open", "High", "Low", "Close", "Volume_USD", fillna=True)
-        # df = add_volume_ta(df, high, low, close, volume, fillna=fillna)
-        # df = add_momentum_ta(df, high, low, close, volume, fillna=fillna)
-        df = add_volatility_ta(df, high, low, close, fillna=fillna)
-        df = add_trend_ta(df, high, low, close, fillna=fillna)
-        df = add_others_ta(df, close, fillna=fillna)
+        volume = "Volume"
+        df = add_all_ta_features(df, open, high, low, close, volume, fillna=True)
 
-        # Momentum TA
+        # # Momentum TA
         df['momentum_rsi'] = rsi(df[close], n=14, fillna=fillna)
         # df['momentum_mfi'] = money_flow_index(df[high], df[low], df[close], df[volume], n=14, fillna=fillna)
         df['momentum_tsi'] = tsi(df[close], r=25, s=13, fillna=fillna)
@@ -45,13 +41,22 @@ while not done:
         df['momentum_stoch_signal'] = stoch_signal(df[high], df[low], df[close], fillna=fillna)
         df['momentum_wr'] = wr(df[high], df[low], df[close], fillna=fillna)
         df['momentum_ao'] = ao(df[high], df[low], fillna=fillna)
+        #
+        # # Volume TA
+        df['volume_adi'] = acc_dist_index(df[high], df[low], df[close], df[volume], fillna=fillna)
+        df['volume_obv'] = on_balance_volume(df[close], df[volume], fillna=fillna)
+        df['volume_obvm'] = on_balance_volume_mean(df[close], df[volume], fillna=fillna)
+        df['volume_cmf'] = chaikin_money_flow(df[high], df[low], df[close], df[volume], n=20, fillna=fillna)
+        df['volume_fi'] = force_index(df[close], df[volume], n=2, fillna=fillna)
+        df['volume_em'] = ease_of_movement(df[high], df[low], df[close], df[volume], n=20, fillna=fillna)
+        df['volume_vpt'] = volume_price_trend(df[close], df[volume], fillna=fillna)
+        df['volume_nvi'] = negative_volume_index(df[close], df[volume], fillna=fillna)
 
         # append to target db
         conn.close()
         connT = sqlite3.connect(targetFile)
         df.to_sql('price_hist', connT, if_exists='append')
         connT.close()
-
     else:
         done = True
 
---
+++ App/Scripts/TA/VolumeAnalysis.py
@@ -0,0 +1,81 @@
+import pandas as pd
+from App.Helpers.AccessTaDB import AccessDB
+import matplotlib.pyplot as plt
+
+
+class VolumeAnalysis:
+
+    def __init__(self):
+        self.db = AccessDB()
+
+    def get_ADI(self, offset, window_size):
+        """
+        Accumulation distribution indicator (ADI) The ADI is an indicator that helps predict reversals
+
+        :param offset: offset of the window
+        :param window_size: size of the window
+        :return: pandas with the column: volume_adi
+        """
+        return self.db.get_window_column(["volume_adi"], offset, window_size)
+
+    def get_OBV(self, offset, window_size):
+        """
+        On-balance volume is a technical analysis indicator intended to relate price and volume in the stock marke
+
+        :param offset: offset of the window
+        :param window_size: size of the window
+        :return: pandas with the column: volume_obv
+        """
+        return self.db.get_window_column(["volume_obv"], offset, window_size)
+
+    def get_OBV_mean(self, offset, window_size):
+        """
+        :param offset: offset of the window
+        :param window_size: size of the window
+        :return: pandas with the column: volume_obvm
+        """
+        return self.db.get_window_column(["volume_obvm"], offset, window_size)
+
+    def get_CMF(self, offset, window_size):
+        """
+        :param offset: offset of the window
+        :param window_size: size of the window
+        :return: pandas with the column: volume_cmf
+        """
+        return self.db.get_window_column(["volume_cmf"], offset, window_size)
+
+    def get_FI(self, offset, window_size):
+        """
+        :param offset: offset of the window
+        :param window_size: size of the window
+        :return: pandas with the column: volume_fi
+        """
+        return self.db.get_window_column(["volume_fi"], offset, window_size)
+
+    def get_EMV(self, offset, window_size):
+        """
+        :param offset: offset of the window
+        :param window_size: size of the window
+        :return: pandas with the column: volume_em'
+        """
+        return self.db.get_window_column(["volume_em"], offset, window_size)
+
+    def get_VPT(self, offset, window_size):
+        """
+        :param offset: offset of the window
+        :param window_size: size of the window
+        :return: pandas with the column: volume_vpt'
+        """
+        return self.db.get_window_column(["volume_vpt"], offset, window_size)
+
+    def get_NVI(self, offset, window_size):
+        """
+        :param offset: offset of the window
+        :param window_size: size of the window
+        :return: pandas with the column: volume_nvi'
+        """
+        return self.db.get_window_column(["volume_nvi"], offset, window_size)
+
+
+v_analysis = VolumeAnalysis()
+print(v_analysis.get_ADI(100, 100))
