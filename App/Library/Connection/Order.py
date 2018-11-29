d02a2a2412e8d2a71fa1411062b1467ef64ba55d
Camiel Kerkhofs <camielk@live.nl>
2018-11-29 18:21:47 +0100

    WIP: place stop loss order and close all positions

--- App/Library/Connection/Order.py
+++ App/Library/Connection/Order.py
@@ -8,4 +8,31 @@ class Order:
     def get_orders():
         return Client.get_connection().get_orders()
 
-    #TODO: add buy/sell methods
+    @staticmethod
+    def all_in():
+        """
+        BUY: Goes all in with the entire portfolio, unless already in this position
+        """
+        conn = Client.get_connection()
+
+        if conn.get_open_positions().T.size == 0:
+            # No open positions, place a stop loss order with the full portfolio
+            accounts = conn.get_accounts()
+            usable_funds = accounts['balance'][0].T
+
+            # Place a buy order
+            trade = conn.create_market_buy_order('EUR/USD', 1000)
+
+            # Set a stop loss rate
+            conn.change_trade_stop_limit(trade['tradeId'], is_in_pips=False, is_stop=True, rate=115)
+            return True
+        else:
+            # Positions are already opened!
+            return True
+
+    @staticmethod
+    def sell_all():
+        """
+        SELL: Closes all open positions
+        """
+        return Client.get_connection().close_all()
--- Test/Connection/TestOrder.py
+++ Test/Connection/TestOrder.py
@@ -6,7 +6,11 @@ from App.Library.Connection.Order import Order
 class TestOrder(unittest.TestCase):
 
     def test_get_order_ids(self):
-        orders = Order.get_orders()
+        # orders = Order.get_orders()
+        self.assertEqual(True, True)
+
+    def test_all_in(self):
+        order = Order.all_in()
         self.assertEqual(True, True)
 
     @classmethod
