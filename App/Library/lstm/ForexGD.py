b6bc764d08df82d1c5444a2c93d69ca812041242
philippe debie <philippe.mmd@gmail.com>
2019-01-15 18:08:09 +0100

    Adding Gradient Decent LSTM

--- App/Library/lstm/ForexGD.py
+++ App/Library/lstm/ForexGD.py
@@ -2,36 +2,33 @@ import random
 
 from App.Library.lstm.ForexBase import *
 
+scaler = 1000.0
 
-class ForexRandom(ForexBase):
+class ForexGD(ForexBase):
 
     def get_X_train(self):
         X = np.zeros((self.batch_size, self.sequence_size, len(self.technical_indicators)))
-        price = np.zeros((self.batch_size, 4))
+        price = np.zeros((self.batch_size, 1))
 
         for batch in range(self.batch_size):
-            offset = int(random.random() * (self.train_size - self.sequence_size - 21))
+            offset = int(random.random() * (self.train_size - self.sequence_size - 10))
 
             X[batch, :, :] = self.TA_train[offset: (offset + self.sequence_size), :]
-            price[batch, 0] = self.price_train[offset + self.sequence_size + 2, 1]
-            price[batch, 1] = self.price_train[offset + self.sequence_size + 5, 1]
-            price[batch, 2] = self.price_train[offset + self.sequence_size + 10, 1]
-            price[batch, 3] = self.price_train[offset + self.sequence_size + 20, 1]
+            finalPrice = self.price_train[offset + self.sequence_size, 1]
+            price[batch, 0] = (self.price_train[offset + self.sequence_size + 10, 1] - finalPrice) * scaler
 
         return X, price
 
-    def get_X_test(self, batch_size):
+    def get_X_test(self):
         X = np.zeros((self.batch_size, self.sequence_size, len(self.technical_indicators)))
-        price = np.zeros((self.batch_size, 4))
+        price = np.zeros((self.batch_size, 1))
 
         for batch in range(self.batch_size):
-            offset = int(random.random() * (self.test_size - self.sequence_size - 21))
+            offset = int(random.random() * (self.test_size - self.sequence_size - 10))
 
             X[batch, :, :] = self.TA_test[offset: (offset + self.sequence_size), :]
-            price[batch, 0] = self.price_test[offset + self.sequence_size + 2, 1]
-            price[batch, 1] = self.price_test[offset + self.sequence_size + 5, 1]
-            price[batch, 2] = self.price_test[offset + self.sequence_size + 10, 1]
-            price[batch, 3] = self.price_test[offset + self.sequence_size + 20, 1]
+            finalPrice = self.price_test[offset + self.sequence_size, 1]
+            price[batch, 0] = (self.price_test[offset + self.sequence_size + 10, 1] - finalPrice) * scaler
 
         return X, price
 
---
+++ App/Library/lstm/lstmGD.py
@@ -0,0 +1,89 @@
+import datetime
+import pickle
+import time
+import traceback
+
+import numpy as np
+import tensorflow as tf
+
+from App.Library.lstm.ForexGD import ForexGD
+
+l1Size = 30
+l2Size = 20
+lstmSize = 20
+outputSize = 1
+sequenceSize = 60
+batchSize = 200
+
+forex = ForexGD(batchSize, sequenceSize, -1, outputSize)
+inputSize = len(forex.technical_indicators)
+
+variables = {
+	'l1': tf.Variable(tf.random_normal([inputSize, l1Size])),
+	'l1b': tf.Variable(tf.random_normal([l1Size])),
+	'l2': tf.Variable(tf.random_normal([l1Size, l2Size])),
+	'l2b': tf.Variable(tf.random_normal([l2Size])),
+	'l3': tf.Variable(tf.random_normal([lstmSize, outputSize])),
+	'l3b': tf.Variable(tf.random_normal([outputSize]))
+}
+
+def check(M, l):
+	if M.get_shape().as_list() != l:
+		print(M.get_shape().as_list())
+		assert False
+
+
+def batchMatMul(M, N):
+	return tf.reshape(tf.reshape(M, [-1, M.get_shape()[-1]]) @ N, [-1, M.get_shape()[-2], N.get_shape()[-1]])
+
+
+def buildNN(x):
+	# x is input of neural network, size: (Batch size * Amount of timesteps * amount of technical indicators)
+
+	# Feed forward layer. (batchMatMul is a TF trick to not have to split it)
+	x = tf.nn.relu(batchMatMul(x, variables['l1']) + variables['l1b'])
+	check(x, [None, sequenceSize, l1Size])
+
+	x = tf.nn.relu(batchMatMul(x, variables['l2']) + variables['l2b'])
+	check(x, [None, sequenceSize, l2Size])
+
+	x = tf.unstack(x, sequenceSize, 1)
+	cell1 = tf.nn.rnn_cell.LSTMCell(lstmSize)
+	outputs, states = tf.nn.static_rnn(cell1, x, dtype=tf.float32)
+	x = tf.stack(outputs, 1)
+	check(x, [None, sequenceSize, lstmSize])
+
+	x = tf.nn.tanh(batchMatMul(x, variables['l3']) + variables['l3b'])
+	check(x, [None, sequenceSize, outputSize])
+
+	x = x[:,-1,:]
+	check(x, [None, outputSize])
+
+	return x
+
+x = tf.placeholder("float", [None, sequenceSize, inputSize])
+y = tf.placeholder("float", [None, outputSize])
+
+Y = buildNN(x)
+
+lossCalc = tf.reduce_mean(tf.square(Y - y))
+
+trainStep = tf.train.AdamOptimizer(1e-4).minimize(lossCalc)
+
+with tf.Session() as sess:
+	sess.run(tf.global_variables_initializer())
+
+	while True:
+		lossList = []
+		priceDiffList = []
+		for i in range(50):
+			X, price = forex.get_X_train()
+			priceDiffList.append(np.mean(price))
+			loss, _ = sess.run([lossCalc, trainStep], feed_dict={x: X, y: price})
+
+			lossList.append(loss)
+
+		X, price = forex.get_X_test()
+		loss = sess.run([lossCalc], feed_dict={x: X, y: price})
+
+		print("Train loss:", np.mean(lossList), "Test loss:", loss, "Avg price diff:", np.mean(priceDiffList))
